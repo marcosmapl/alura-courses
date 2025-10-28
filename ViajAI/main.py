c2881fa0864b67214ab377c655976684f3dda2c6
Marcos Pimenta <marcos.pimenta@manaus.am.gov.br>
2025-10-28 15:54:46 -0400

    feat(prompt): add PrompTemplate

--- ViajAI/main.py
+++ ViajAI/main.py
@@ -3,6 +3,7 @@ Python script to generate a personalized travel itinerary using OpenAI's API.
 """
 # from openai import OpenAI
 from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 import os
 
@@ -16,15 +17,34 @@ interests = input("Quais são os interesses da sua família? (ex: aventura, cult
 period = input("Digite o(s) mês(es) da viagem: ")
 budget = input("Digite o orçamento aproximado da viagem (em R$): ")
 
-prompt = f"Gere um roteiro de viagem de {n_days} dias para {n_adults} adultos e {n_children} crianças em {destination}, com foco em {interests}, durante o período de {period} e com orçamento de R$ {budget}. Inclua atividades diárias, opções de alimentação, hospedagem, dicas práticas e um resumo com ritmo e custo estimado."
-
-client = ChatOpenAI(
-    model="gpt-3.5-turbo",
-    temperature=0.7,
-    api_key=os.getenv('OPENAI_API_KEY')
+template = PromptTemplate(
+    tmplate="""
+    Crie um roteiro de viagem para uma família de {n_adults} adultos e {n_children} crianças.
+    A viagem terá como destino {destination}, duração de {n_days} dias nos meses {period}.
+    A família tem interesse em {interests} e pretende gastar até R$ {budget} na viagem.
+    Inclua atividades diárias, opções de alimentação, hospedagem, dicas práticas e um resumo com ritmo e custo estimado.
+    """
 )
 
-response = client.invoke(prompt)
+prompt = template.format(
+    n_adults=n_adults,
+    n_children=n_children,
+    destination=destination,
+    n_days=n_days,
+    interests=interests,
+    period=period,
+    budget=budget
+)
 
-print("Roteiro de Viagem:")
-print(response.content)
+print("Prompt gerado:", prompt)
+
+# client = ChatOpenAI(
+#     model="gpt-3.5-turbo",
+#     temperature=0.7,
+#     api_key=os.getenv('OPENAI_API_KEY')
+# )
+
+# response = client.invoke(prompt)
+
+# print("Roteiro de Viagem:")
+# print(response.content)
