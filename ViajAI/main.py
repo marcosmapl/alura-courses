9ddd7eb8524600ea0206b2cc17e2137e0b9cc96e
Marcos Pimenta <marcos.pimenta@manaus.am.gov.br>
2025-10-29 09:03:32 -0400

    feat(chain): example showing how to concatenate multiple chains into a simple pipeline

--- ViajAI/main.py
+++ ViajAI/main.py
@@ -2,49 +2,80 @@
 Python script to generate a personalized travel itinerary using OpenAI's API.
 """
 # from openai import OpenAI
-from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from langchain_core.globals import set_debug
+from pydantic import BaseModel, Field
+
 import os
 
+
+class Destino(BaseModel):
+    cidade: str = Field("A cidade recomendada para visitar.")
+    motivo: str = Field("O motivo pelo qual a cidade foi recomendada.")
+
+
+class Restaurantes(BaseModel):
+    cidade: str = Field("A cidade recomendada para visitar.")
+    restaurantes: list[str] = Field("Uma lista com os nomes dos restaurantes recomendados na cidade.")
+
+
+set_debug(True)
 load_dotenv()
 
-n_days = input("Digite o número de dias da viagem: ")
-n_adults = input("Digite o número de adultos na família: ")
-n_children = input("Digite o número de crianças na família: ")
-destination = input("Digite o destino da viagem: ")
-interests = input("Quais são os interesses da sua família? (ex: aventura, cultura, gastronomia): ")
-period = input("Digite o(s) mês(es) da viagem: ")
-budget = input("Digite o orçamento aproximado da viagem (em R$): ")
+# Initialize the JSON output parser
+parser_destino = JsonOutputParser(pydantic_object=Destino)
+parser_restaurante = JsonOutputParser(pydantic_object=Restaurantes)
 
-template = PromptTemplate(
-    tmplate="""
-    Crie um roteiro de viagem para uma família de {n_adults} adultos e {n_children} crianças.
-    A viagem terá como destino {destination}, duração de {n_days} dias nos meses {period}.
-    A família tem interesse em {interests} e pretende gastar até R$ {budget} na viagem.
-    Inclua atividades diárias, opções de alimentação, hospedagem, dicas práticas e um resumo com ritmo e custo estimado.
-    """
+# Define the prompt template
+prompt_destino = PromptTemplate(
+    template="""
+    Sugira uma cidade para visitar dado o meu interesse por {interests}.
+    {output_format}
+    """,
+    input_variables=["interests"],
+    partial_variables={'output_format': parser_destino.get_format_instructions()}
 )
 
-prompt = template.format(
-    n_adults=n_adults,
-    n_children=n_children,
-    destination=destination,
-    n_days=n_days,
-    interests=interests,
-    period=period,
-    budget=budget
+prompt_restaurante = PromptTemplate(
+    template="""
+    Sugira restaurantes populares da cidade {cidade}.
+    {output_format}
+    """,
+    partial_variables={'output_format': parser_restaurante.get_format_instructions()}
 )
 
-print("Prompt gerado:", prompt)
+prompt_cultural = PromptTemplate(
+    template="""Sugira atividades culturais para fazer em {cidade}."""
+)
 
-# client = ChatOpenAI(
-#     model="gpt-3.5-turbo",
-#     temperature=0.7,
-#     api_key=os.getenv('OPENAI_API_KEY')
-# )
+# Initialize the OpenAI client
+model = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0.7,
+    api_key=os.getenv('OPENAI_API_KEY')
+)
 
-# response = client.invoke(prompt)
+# Collect user inputs
+interests = input("Quais são os interesses da sua viagem? (ex: aventura, cultura, gastronomia): ")
 
-# print("Roteiro de Viagem:")
-# print(response.content)
+# Define the variables for the prompt
+vars = {
+    "interests": interests,
+}
+
+# Create the chains
+chain_destino = prompt_destino | model | parser_destino
+chain_restaurante = prompt_restaurante | model | parser_restaurante
+chain_cultural = prompt_cultural | model | StrOutputParser()
+
+chain_viajai = (chain_destino | chain_restaurante | chain_cultural)
+
+# Invoke the chain with user inputs
+response = chain_viajai.invoke(
+    vars
+)
+
+print(response)
